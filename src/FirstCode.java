6c7a9f98c2bff4765e706399f11a3d79595ef19e
Shehryar Sajjad <shehryar.sajjad@yahoo.com>
2019-07-03 02:48:09 +0200

    Initial Commit learning git hub

---
+++ src/FirstCode.java
@@ -0,0 +1,11 @@
+public class FirstCode {
+    public static void main(String args[])
+    {
+     short num3 = 5; // 2 bytes -> 16 bits -32768 to 32768
+     int num1 = 5; // 4 bytes -> 32bits
+     num1 = 8;
+     System.out.print(num1);
+     double num2 = 5.5;
+     System.out.print(num2);
+    }
+}
