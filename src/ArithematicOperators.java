b73d6d107d455d851d7df5af48d7ce7c554dd01a
Shehryar Sajjad <shehryar.sajjad@yahoo.com>
2019-07-07 03:41:21 +0200

    Naming Convenctions and Arithematic Operators

---
+++ src/ArithematicOperators.java
@@ -0,0 +1,25 @@
+public class ArithematicOperators {
+    /*
+    Arithematic
+
+
+    */
+
+    public static void main(String[] args)
+    {
+        int m=2,n=4;
+        //double r1= (double) m/n;
+        //int r2 = m%n;
+        //n++ // n+=1 and n = n+1;
+        /*
+        n++ post increment
+        ++n pre increment
+         */
+        m = n++;
+        //m = ++n;
+        System.out.print(m); // 4
+        System.out.print(n); // 5
+
+
+    }
+}
--- src/FirstCode.java
+++ src/FirstCode.java
@@ -1,11 +1,17 @@
 public class FirstCode {
     public static void main(String args[])
     {
+
+     long l = 50000000000l; // 8 bytes
      short num3 = 5; // 2 bytes -> 16 bits -32768 to 32768
      int num1 = 5; // 4 bytes -> 32bits
-     num1 = 8;
-     System.out.print(num1);
      double num2 = 5.5;
-     System.out.print(num2);
+     double INTTRY= 5; // it can store int but will return 5.0 as double stores decimal values
+     int DOUBLETRY  = (int)5.6; // This is called type casting we don't need .6 Also called Explicit conversions
+
+     num1 = 8;
+     //System.out.print(num1);
+     //System.out.print(num2);
+     System.out.print(l);
     }
 }
