e4741eab91aaa1ed721c6c705c1847f26b7a6ca6
Shehryar Sajjad <shehryar.sajjad@yahoo.com>
2019-11-10 19:14:15 +0100

    Inner Class and Static Inner Class

--- src/InnerClass.java
+++ src/InnerClass.java
@@ -1,9 +1,39 @@
-
-public class InnerClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Outer 
+{
+	int a;
+	public void show()
+	{
+		System.out.println("In Display Outer Class");
+	}
+	class Inner
+	{
+		public void display()
+		{
+			System.out.println("In Display Inner Class");
+		}
+	
+	}
+	static class InnerStatic
+	{
+		public void display()
+		{
+			System.out.println("In Display Static Class");
+		}
+	
+	}
+}
+public class InnerClass 
+{
+	
+	public static void main(String[] args) 
+	{
+		Outer obj = new Outer();
+		obj.show();
+		Outer.Inner obj1 = obj.new Inner(); // We need Outer class object for non static class.
+		Outer.InnerStatic obj2 = new Outer.InnerStatic(); // We don't need object of outer class to create static inner class object. 
+		obj1.display();
+		obj2.display();
+		
 	}
 
-}
+}  
