90967d0a675757b816f9f80963165c9f9db9fd40
Shehryar Sajjad <shehryar.sajjad@yahoo.com>
2019-11-11 02:18:48 +0100

    VarArgs Need practice bit confusing

---
+++ src/Varargs.java
@@ -0,0 +1,26 @@
+class Calc 
+{
+		public int add(int ... n) // Will be used when you don't know how much values will be passed in the method.
+		{
+			int sum = 0;
+			for (int i : n)
+			{
+				sum = sum + i;
+			}
+			return sum;
+		}
+
+}
+
+
+public class Varargs {
+
+	public static void main(String[] args) 
+	{
+		Calc obj = new Calc();
+		System.out.println(obj.add(10, 8, 9, 10));
+		
+
+	}
+
+}
