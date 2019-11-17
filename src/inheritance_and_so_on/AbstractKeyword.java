f75358fd790c2b986f06a5323f95ea1b525c8bf5
Shehryar Sajjad <shehryar.sajjad@stud.uni-bamberg.de>
2019-11-17 17:40:15 +0100

    Abstract Class

---
+++ src/inheritance_and_so_on/AbstractKeyword.java
@@ -0,0 +1,32 @@
+package inheritance_and_so_on;
+/*
+ * A class that may be extended in future but for now its empty and we don't want anyone to create object of
+ * class directly. If we use the Keyword Abstract then no one will be able to create its object directly. 
+ * But some other class can extend this class then we can reference the abstract class and create object with
+ * the help of other class as show in the example.
+ */
+abstract class A  
+	{
+		public abstract void display(); // Method declared but not defined
+	}
+
+class B extends A
+	{
+		public void display() // defined the super class method and it is must otherwise class B would also 
+							// abstract.
+		{	
+			System.out.println("Implemented Abstract Method");
+		}
+	}
+
+
+public class AbstractKeyword {
+
+	public static void main(String[] args) 
+	{
+		A a1 = new B();
+		a1.display();
+
+	}
+
+}
