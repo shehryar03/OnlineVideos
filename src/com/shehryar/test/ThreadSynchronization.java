c4ea8fc1601aa2510f08c6783ccaf315b0443e02
Shehryar Sajjad <shehryar.sajjad@stud.uni-bamberg.de>
2019-12-10 01:37:32 +0100

    How to keep the threads in sync by using sychronized keyword in method

---
+++ src/com/shehryar/test/ThreadSynchronization.java
@@ -0,0 +1,52 @@
+package com.shehryar.test;
+
+class Counter
+{
+	int count;
+	
+	public synchronized void increment() // synchoronized keyword is used because to keep the threads in sync
+										// While they access the increment method
+	{
+		count ++;
+	}
+}
+
+public class ThreadSynchronization {
+
+	public static void main(String[] args) throws Exception
+	{
+		Counter c = new Counter();
+		Thread t1 = new Thread(new Runnable()
+				{	
+					public void run()
+					{
+						for(int i=1;i<=1000;i++)
+						{
+							c.increment();
+						}
+					}
+				}
+				
+				); 
+		Thread t2 = new Thread(new Runnable()
+		{	
+			public void run()
+			{
+				for(int i=1;i<=1000;i++)
+				{
+					c.increment();
+				}
+			}
+		}
+		
+		); 
+		
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println(c.count);
+		
+	}
+
+}
