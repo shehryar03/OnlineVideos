abb8467f49a5344c3e95111129b3e2b26e693830
Shehryar Sajjad <shehryar.sajjad@stud.uni-bamberg.de>
2019-12-26 04:54:42 +0100

    Revised Switch if else and Ternary Operator

--- src/SelectionStatements.java
+++ src/SelectionStatements.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class SelectionStatements {
 
     public static void main(String[]args){
@@ -8,23 +9,25 @@ public class SelectionStatements {
         if (n%2==0)
         {   System.out.print("Even");}
         else
-        {   System.out.print("Odd");}
+        {   System.out.print("Odd");}*/
 
         int i=9, j=1;
         j=i>7?7:9; // Ternery statement alternate to if else
         System.out.print(j);
-        */
-        char a = 'A';
-        switch (a) /// also supports String Interview Question
+       /* 
+    	Scanner obj = new Scanner(System.in); // Stream opener object
+    	String n = obj.nextLine(); // String input object
+    	String convertedToString = n.toString(); // parsing of String obj into string variable
+        switch (convertedToString) /// also supports String Interview Question
         {
-            case 'A':
+            case "A":
                 System.out.print("A for apple");
                 break;
-            case 'B':
+            case "B":
                 System.out.print("B for Ball");
                 break;
             default:
                 System.out.print("Nothing");
-        }
+        } */
     }
 }
