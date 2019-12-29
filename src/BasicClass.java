f213a4ef067baeaf91c7bf9942aed03b9da70438
Shehryar Sajjad <shehryar.sajjad@stud.uni-bamberg.de>
2019-12-29 21:41:53 +0100

    Classes and Objects Revision

---
+++ src/BasicClass.java
@@ -0,0 +1,36 @@
+// Class is a blue print that is used to create the object.
+// Object knows something (data) and does something (perform actions).
+// To access Variable and Method we need Objects.
+
+// A Constructor is also a member method of class it has the same name as class name.
+// A doesn't return anything. e.g Calculator()
+class Calculator 
+{
+		int num1;
+		int num2;
+		int result;
+		
+	public void display() 
+	{
+			result = num1 + num2 ;
+	}
+	
+}
+
+public class BasicClass {
+
+	public static void main(String[] args) // 1st statement that is executed when a program starts
+	{
+		Calculator obj = new Calculator(); // knows something and does something
+		// knows means the data the variable and does something means the methods
+		// new is the keyword that is used to instanciate an object basically it provides space (memory)
+		// and next is the Constructor e.g Calc(); which tells us how much memory that object will occupy.
+		// OR you can say as Constructor is used to allocate the memory to object.
+		obj.num1 = 10;
+		obj.num2 = 10;
+		obj.display();
+		System.out.println(obj.result);
+		
+	}
+
+}
