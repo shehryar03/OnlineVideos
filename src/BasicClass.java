b6e55c4372f7f910b99f9b7eef19fd94472554cc
Shehryar Sajjad <shehryar.sajjad@stud.uni-bamberg.de>
2019-12-29 22:11:39 +0100

    improved with param constructor

--- src/BasicClass.java
+++ src/BasicClass.java
@@ -15,19 +15,30 @@ class Calculator
 			result = num1 + num2 ;
 	}
 	
+	Calculator() // Can be used to initialize the variable
+	{
+		num1 = 10;
+		num2 = 10;
+	}
+	
+	Calculator(int i, int j) // Parameterized Constructor
+	{
+		num1 = i;
+		num2 = j;
+	}
 }
 
 public class BasicClass {
 
 	public static void main(String[] args) // 1st statement that is executed when a program starts
 	{
-		Calculator obj = new Calculator(); // knows something and does something
+		Calculator obj = new Calculator(7,7); // knows something and does something
 		// knows means the data the variable and does something means the methods
-		// new is the keyword that is used to instanciate an object basically it provides space (memory)
+		// new is the keyword that is used to instantiate an object basically it provides space (memory)
 		// and next is the Constructor e.g Calc(); which tells us how much memory that object will occupy.
 		// OR you can say as Constructor is used to allocate the memory to object.
-		obj.num1 = 10;
-		obj.num2 = 10;
+		//obj.num1 = 10;
+		//obj.num2 = 10;
 		obj.display();
 		System.out.println(obj.result);
 		
