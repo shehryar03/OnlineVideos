8edb8659792222a1261da46c9ef3ee5694e532c6
Shehryar Sajjad <shehryar.sajjad@stud.uni-bamberg.de>
2020-01-07 02:22:09 +0100

    ll

--- src/inheritance_and_so_on/AbstractKeyword.java
+++ src/inheritance_and_so_on/AbstractKeyword.java
@@ -3,7 +3,7 @@ package inheritance_and_so_on;
  * A class that may be extended in future but for now its empty and we don't want anyone to create object of
  * class directly. If we use the Keyword Abstract then no one will be able to create its object directly. 
  * But some other class can extend this class then we can reference the abstract class and create object with
- * the help of other class as show in the example.
+ * the help of other class as show in the example //.
  */
 abstract class A  
 	{
